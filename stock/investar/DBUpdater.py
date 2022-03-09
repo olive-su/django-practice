6ec8de3c0ba8cc9607ce88a209bb5a5bda2655b8
olive-su <1466su@naver.com>
2022-03-09 15:38:59 +0900

    Update:기업 목록 업데이트

--- stock/investar/DBUpdater.py
+++ stock/investar/DBUpdater.py
@@ -159,7 +159,7 @@ class DBUpdater:
                 pages_to_fetch = config['pages_to_fetch']
         except FileNotFoundError:
             with open('config.json', 'w') as out_file:
-                pages_to_fetch = 10
+                pages_to_fetch = 20
                 config = {'pages_to_fetch': 1}
                 json.dump(config, out_file)
         self.update_daily_price(pages_to_fetch)
